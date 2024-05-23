671db493616affe1b6b14d467b1fc7832720f0a0
kwon <kckc0608@naver.com>
2024-05-23 16:26:11 +0900

    다른 회사의 authoritative dns server 추가

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
-@REM start "abcdef company DNS" python companyDNSserver.py 10001 ^<abcdef.txt^>
+start "xyz company DNS" python companyDNSserver.py 10002 ^<xyz.txt^>
+start "abcdef company DNS" python companyDNSserver.py 10001 ^<abcdef.txt^>
 start "xy company DNS" python companyDNSserver.py 10000 ^<xy.txt^>
 start "com TLD DNS" python comTLDDNSserver.py 23004
 start "root_DNS" python rootDNSserver.py 23003
